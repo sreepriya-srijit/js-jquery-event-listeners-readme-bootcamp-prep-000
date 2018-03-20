8d7e6a67ae8fd71fe23dfefce771b9ffffaf5e10
Sreepriya Srijit <sreepriyakurup87@gmail.com>
2018-03-20 00:57:55 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
 //define functions here
-
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+submitIt();
+pressIt();
 
 });
+
+function getIt(){
+  $('p').on('click',function(){
+    alert("Hey!");
+  });
+}
+
+function frameIt(){
+  $('img').on('load',function(){
+    $(this).addClass('tasty');
+  })
+}
+
+function submitIt(){
+  $('form').on('submit',function(){
+    alert('Your form is going to be submitted now.');
+  });
+}
+
+function pressIt(){
+  $('#typing').on('keydown', function(key){
+    if(key.which == 71 )
+    {
+      alert('You pressed G');
+    }
+    
+  });
+}
